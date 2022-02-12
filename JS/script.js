177c65fabadb5889ba15b5cc25a8c5f2adb51e48
Norbit <iskander5577@mail.ru>
2022-02-12 18:18:53 +0600

    first commit

---
+++ JS/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+let number = 5.7;
+console.log(-4/0);
+
+const person = 'Yozhik';
+const bool = true;
+
+let und;
+console.log(und);
+const obj = {
+    name: 'Yozhik',
+    age: 18,
+    isFromYokshir: true
+};
+console.log(obj.isFromYokshir);
---
+++ JS/yozhik.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// alert('Hello');
+// const result = confirm('are you idiot');
+// console.log(result);
+// const answer = +prompt('how many yozhics do you have', '');
+// console.log(answer);
+// console.log(typeof(answer));
+
+const answers = [];
+
+// answers[0] = prompt('what is your name', '');
+// answers[1] = prompt('What is your surname', '');
+// answers[2] = prompt('how old are you', '');
+
+console.log(typeof(answers));

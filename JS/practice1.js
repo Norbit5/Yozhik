a89ba1981b18bc3c51bd1df1e54dd6f0ae333721
Iskander <iskander5577@mail.ru>
2022-02-13 18:30:31 +0600

    task2

--- JS/practice1.js
+++ JS/practice1.js
@@ -2,3 +2,12 @@
 let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', "");
 console.log(numberOfFilms);
 
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+console.log(personalMovieDB);
+

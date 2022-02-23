388b457b689bd7ae15cf8de30235405c8a251a3c
Iskander <iskander5577@mail.ru>
2022-02-23 20:53:06 +0600

    practice2

--- JS/practice2.js
+++ JS/practice2.js
@@ -1,15 +1,3 @@
-'use strict';
-let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', "");
-
-let personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false,
-};
-console.log(personalMovieDB);
-
 if (personalMovieDB.count < 10) {
     alert("Просмотренно довольно мало фильмов");
 } else if (personalMovieDB.count >=10 && personalMovieDB.count <=30) {
@@ -19,26 +7,3 @@ if (personalMovieDB.count < 10) {
 } else {
     alert('Произошла ошибка');
 }
-
-
-
-
-
-
-
-
-
-
-
-
-// for (let i = 1; i <= numberOfFilms; i++) {
-//     let a = prompt('Один из последних просмотренных фильмов', '');
-//     let b = prompt('На сколько оцените его', '');
-//     if (a == '' || b == '' || a == null || b == null || a.length > 50)  {
-//         alert('не тупи');
-//         i--;
-//     } else {
-//         personalMovieDB.movies[a] = b;
-//     }
-// }
-// console.log(personalMovieDB);
